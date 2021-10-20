3d0bd881d18d159eca575cc3bcfb198fa60e09bd
Ana Katarine de Freitas Santana Santos <ana.k.santana.santos@avanade.com>
2021-10-20 17:26:05 -0300

    ajuste app module

--- sistema-clinica/src/app/app.module.ts
+++ sistema-clinica/src/app/app.module.ts
@@ -1,15 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 import { HeaderModule } from './shared/header/header.module';
 import { FooterModule } from './shared/footer/footer.module';
-import {MatCardModule} from '@angular/material/card';
-import {MatFormFieldModule} from '@angular/material/form-field';
-import {MatButtonModule} from '@angular/material/button';
 import { AuthService } from './auth.service';
 
 import { SharedModule } from 'src/app/shared/shared.module';
@@ -23,13 +19,8 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     BrowserModule,
     HeaderModule,
     FooterModule,
-    AppRoutingModule,
-    MatCardModule,
-    MatFormFieldModule,
-    MatButtonModule,
-    ReactiveFormsModule,
-    FormsModule,
-    BrowserAnimationsModule,
+    SharedModule,
+    BrowserAnimationsModule
   ],
   providers: [AuthService],
   bootstrap: [AppComponent]
