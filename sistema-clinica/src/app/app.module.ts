6f3928df7a4c1b9743e821cf746c00bd9c9602e3
Herbet Junior <herbetjr@gmail.com>
2021-10-21 12:54:39 -0300

    dsdsd

--- sistema-clinica/src/app/app.module.ts
+++ sistema-clinica/src/app/app.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LoginComponent } from './pages/login/login.component';
+import { LoginComponent } from './pages/recepcao/login/login.component';
 import { HttpClientModule } from '@angular/common/http';
 
 import { SharedModule } from 'src/app/shared/shared.module';
