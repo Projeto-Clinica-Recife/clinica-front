aa47659f024db59c8dfe4c6c7359525eb52e9c1e
Ana Katarine de Freitas Santana Santos <ana.k.santana.santos@avanade.com>
2021-10-20 22:24:16 -0300

    projeto sem quebrar

--- sistema-clinica/src/app/app.module.ts
+++ sistema-clinica/src/app/app.module.ts
@@ -11,6 +11,21 @@ import { AuthService } from './auth.service';
 import { SharedModule } from 'src/app/shared/shared.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { MatTableModule } from '@angular/material/table';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatSelectModule } from '@angular/material/select';
+import { MatOptionModule } from '@angular/material/core';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -20,7 +35,21 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     HeaderModule,
     FooterModule,
     SharedModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatSidenavModule,
+    MatToolbarModule,
+    MatMenuModule,
+    MatGridListModule,
+    MatButtonModule,
+    MatInputModule,
+    MatCardModule,
+    MatIconModule,
+    MatTableModule,
+    MatDividerModule,
+    MatSlideToggleModule,
+    MatSelectModule,
+    MatOptionModule,
+    MatProgressSpinnerModule,
   ],
   providers: [AuthService],
   bootstrap: [AppComponent]
--- sistema-clinica/src/app/shared/shared.module.ts
+++ sistema-clinica/src/app/shared/shared.module.ts
@@ -6,20 +6,6 @@ import { FooterComponent } from "./footer/footer/footer.component";
 import { HeaderComponent } from "./header/header/header.component";
 
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatSidenavModule } from '@angular/material/sidenav';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatMenuModule } from '@angular/material/menu';
-import { MatGridListModule } from '@angular/material/grid-list';
-import { MatButtonModule } from '@angular/material/button';
-import { MatInputModule } from '@angular/material/input';
-import { MatCardModule } from '@angular/material/card';
-import { MatIconModule } from '@angular/material/icon';
-import { MatTableModule } from '@angular/material/table';
-import { MatDividerModule } from '@angular/material/divider';
-import { MatSlideToggleModule } from '@angular/material/slide-toggle';
-import { MatSelectModule } from '@angular/material/select';
-import { MatOptionModule } from '@angular/material/core';
-import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 
 const components = [
   FooterComponent,
@@ -31,21 +17,7 @@ const imports = [
   RouterModule,
   FormsModule,
   ReactiveFormsModule,
-  BrowserAnimationsModule,
-  MatSidenavModule,
-  MatToolbarModule,
-  MatMenuModule,
-  MatGridListModule,
-  MatButtonModule,
-  MatInputModule,
-  MatCardModule,
-  MatIconModule,
-  MatTableModule,
-  MatDividerModule,
-  MatSlideToggleModule,
-  MatSelectModule,
-  MatOptionModule,
-  MatProgressSpinnerModule,
+  BrowserAnimationsModule
 ];
 
 @NgModule({
